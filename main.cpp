912d702bf58f4c8162e312c9cece359aadd84ecc
Alp Cevikel <acevikel@usc.edu>
2019-04-17 16:13:45 -0700

    options moved as a member struct

--- main.cpp
+++ main.cpp
@@ -838,11 +838,8 @@ int main(int argc, char** argv) {
         if (qpResult.size()!=combinedQP.x.size()){
           qpResult.resize(combinedQP.x.size());
         }
-        qpOASES::Options options;
-        options.setToMPC();
-        options.printLevel = qpOASES::PL_NONE;
-        QPOASESOptions qp_options = QPOASESOptions(set_max_time,dt,1000, options);
-        QPOASESSolver<double> qpSolver(qp_options);
+        
+        QPOASESSolver<double> qpSolver(set_max_time,dt);
         bool qp_succeded = qpSolver.solve(combinedQP, qpResult);
         
         if (qp_succeded) {
--- solvers/QPOASESSolver.h
+++ solvers/QPOASESSolver.h
@@ -2,42 +2,52 @@
 #include "QPSolver.h"
 #include "spline.h"
 
-struct QPOASESOptions {
-  bool set_max_time;
-  qpOASES::real_t cputime; 
-  qpOASES::int_t nWSR;
-  qpOASES::Options qpOptions;
 
-  QPOASESOptions(bool set_max_time, qpOASES::real_t cputime, qpOASES::int_t nWSR, qpOASES::Options qpOptions){
-    this->set_max_time = set_max_time;
-    this-> cputime = cputime;
-    this-> nWSR = nWSR;
-    this-> qpOptions = qpOptions;
-  }
-};
-
+namespace ACT {
 template<class T>
 class QPOASESSolver : public QPSolver<T> {
-   private:
-   using QPMatrices = typename Spline<T, 3U>::QPMatrices;
-   using Vector = typename Eigen::Matrix<T, Eigen::Dynamic, 1>;
-   bool set_max_time;
-   qpOASES::Options options;
-   qpOASES::real_t cputime;
-   qpOASES::int_t nWSR;
 
-   public:
-   QPOASESSolver(QPOASESOptions opt) {
-    this->set_max_time = opt.set_max_time;
-    this->options = opt.qpOptions;
-    this->nWSR = opt.nWSR;
-    this->cputime = opt.cputime;
+  public:
+  struct QPOASESOptions {
+    bool set_max_time;
+    qpOASES::real_t cputime; 
+    qpOASES::int_t nWSR;
+    qpOASES::Options qpOptions;
+    QPOASESOptions() {}
+    QPOASESOptions(bool set_max_time, qpOASES::real_t cputime, qpOASES::int_t nWSR, qpOASES::Options qpOptions){
+      this->set_max_time = set_max_time;
+      this-> cputime = cputime;
+      this-> nWSR = nWSR;
+      this-> qpOptions = qpOptions;
+    }
+
+    qpOASES::Options& getQPoptions() {
+      return qpOptions;
+    }
+
+  };
+
+  private:
+  QPOASESOptions mQPOASESOptions;
+  using QPMatrices = typename Spline<T, 3U>::QPMatrices;
+  using Vector = typename Eigen::Matrix<T, Eigen::Dynamic, 1>;
+
+  public:
+  QPOASESSolver(QPOASESOptions opt) {
+    this->mQPOASESOptions = QPOASESOptions(opt.set_max_time,opt.cputime,opt.nWSR,opt.qpOptions);
+   }
+
+   QPOASESSolver (qpOASES::real_t cputime, bool set_max_time) {
+     qpOASES::Options options;
+     options.setToMPC();
+     options.printLevel = qpOASES::PL_NONE;
+     this->mQPOASESOptions = QPOASESOptions(set_max_time,cputime,1000,options);
    }
 
    bool solve(const QPMatrices& combinedQP, Vector& result) override {
      assert(result.rows()==combinedQP.x.rows());
      qpOASES::QProblem problem(combinedQP.x.rows(), combinedQP.A.rows());
-      problem.setOptions(this->options);
+      problem.setOptions(this->mQPOASESOptions.qpOptions);
         qpOASES::returnValue ret =
           problem.init(
             combinedQP.H.data(),
@@ -47,8 +57,8 @@ class QPOASESSolver : public QPSolver<T> {
             combinedQP.ubX.data(),
             combinedQP.lbA.data(),
             combinedQP.ubA.data(),
-            this->nWSR,
-            this->set_max_time ? &this->cputime: NULL,
+            this->mQPOASESOptions.nWSR,
+            this->mQPOASESOptions.set_max_time ? &this->mQPOASESOptions.cputime: NULL,
             combinedQP.x.data()
           );
         qpOASES::int_t simpleStatus = qpOASES::getSimpleStatus(ret);
@@ -58,30 +68,31 @@ class QPOASESSolver : public QPSolver<T> {
    };
 
    bool getSetMaxTime(){
-     return this->set_max_time;
+     return this->mQPOASESOptions.set_max_time;
    }
 
    void setSetMaxTime(bool smt){
-     this->set_max_time = smt;
+     this->mQPOASESOptions.set_max_time = smt;
    }
 
    qpOASES::real_t getCputime(){
-     return this->cputime;
+     return this->mQPOASESOptions.cputime;
    }
 
    void setCputime( qpOASES::real_t tm){
-     this->cputime = tm;
+     this->mQPOASESOptions.cputime = tm;
    }
 
    qpOASES::int_t getNWSR(){
-     return this->nWSR;
+     return this->mQPOASESOptions.nWSR;
    }
 
    void setNWSR(qpOASES::int_t nwsr){
-     this->nWSR = nwsr;
+     this->mQPOASESOptions.nWSR = nwsr;
    }
 
-
-
-
+   qpOASES::Options& getqpOASESOptions() {
+     return (this->mQPOASESOptions.getQPoptions());
+   }
+};
 };
