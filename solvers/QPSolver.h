9d63a5e793b3d06c904db7ee2c3ed7a21dbb31de
Baskin Senbaslar <basbursen@gmail.com>
2020-04-26 10:35:14 -0700

    solvers removed

--- solvers/QPSolver.h
+++
@@ -1,9 +0,0 @@
-#include "spline.h"
-
-template<class T>
-class QPSolver {
-   public:
-   using Vector = typename Eigen::Matrix<T, Eigen::Dynamic, 1>;
-   using QPMatrices = typename Spline<T, 3U>::QPMatrices;
-   virtual bool solve(const QPMatrices& qp, Vector& result) = 0;
-};
