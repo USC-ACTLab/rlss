4b1924ad512d7320cd139997b4a8fa7c81829689
Jiating Lu <jiatinglu99@gmail.com>
2021-10-18 11:58:17 -0700

    implemented SVMSequence const_iterator

--- include/rlss/SVMSequence/SVMSequence.hpp
+++ include/rlss/SVMSequence/SVMSequence.hpp
@@ -12,51 +12,67 @@ template<typename T, unsigned int DIM>
 class SVMSequence {
 public:
     using Hyperplane = rlss::internal::Hyperplane<T, DIM>;
-    using Pair = std::pair<T, std::shared_ptr<Hyperplane>>;
+    using Pair = std::pair<T, Hyperplane>;
     SVMSequence(){
         
     }
     ~SVMSequence(){
 
     }
-    
-    bool add(T _t, Hyperplane* plane){
-        try{
-            sequence.push_back(std::make_pair(_t, std::shared_ptr<Hyperplane>(plane)));
+    bool add(T time, Hyperplane& plane){
+        if (sequence.empty() || sequence.front()->first < time){
+            sequence.push_back(std::make_pair(time, plane));
             return true;
-        } catch(...){
-            return false;
         }
+        else return false;
     }
     bool forget(T _t){
-        try{
-            // TODO is it greater or less?
+        if (sequence.empty()) return false;
+        else{
             while (sequence.front()->first < _t){
-                sequece.pop_front().second.reset();
+                sequece.pop_front().second
             }
             return true;
-        } catch(...) {
-            return false;
         }
     }
-    // TODO implement custom iterator, SVMSequenc::iterator
+    size_type size() const {
+        return sequence.size();
+    }
+    Hyperplane& operator[](size_type i){
+        assert(i < sequence.size());
+        return sequence[i].second;
+    }
+    const Hyperplane& operator[](size_type i) const {
+        assert(i < sequence.size());
+        return sequence[i].second;
+    }
+    // implement custom iterator, SVMSequenc::const_iterator
     // following template found: https://gist.github.com/jeetsukumaran/307264
-    class iterator{
+    class const_iterator{
     public:
-        typedef iterator self_type;
+        typedef const_iterator self_type;
         typedef Hyperplane valueType;
         typedef Hyperplane& reference;
         typedef Hyperplane* pointer;
+        typedef std::deque<Pair>::const_iterator base_iterator;
         typedef std::forward_iterator_tag iterator_category;
         typedef int difference_type;
-        iterator(pointer ptr) : ptr_(ptr) { }
-        self_type operator++(){
-            
-        }
+        const_iterator(base_iterator itr):iterator_to_deque(itr) {}
+        self_type operator++(){++iterator_to_deque; return *this;}
+        self_type operator++(int index){iterator_to_deque += index; return *this;}
+        const reference operator*() {return iterator_to_queue.second;}
+        const pointer operator->() {return iterator_to_queue.second;}
+        bool operator==(const self_type& rhs) {return iterator_to_deque == rhs.iterator_to_deque;}
+        bool operator!=(const self_type& rhs) {return iterator_to_deque != rhs.iterator_to_deque;}
     private:
-        pointer ptr_;
+        base_iterator iterator_to_deque;
     }
-
+    const_iterator cbegin() const {
+        return const_iterator(sequence.cbegin());
+    }
+    const_iterator cend() const {
+        return const_iterator(sequence.cend());
+    }
 private:
     std::deque<Pair> sequence;
 } // class SVMSequence
